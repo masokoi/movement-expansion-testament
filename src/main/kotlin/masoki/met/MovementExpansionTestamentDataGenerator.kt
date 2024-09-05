49cd9d8c02726364ff9171b321d5f9dc997535f3
why <101954856+whyllay@users.noreply.github.com>
2024-09-04 18:53:48 -0600

    Initial commit

---
+++ src/main/kotlin/masoki/met/MovementExpansionTestamentDataGenerator.kt
@@ -0,0 +1,9 @@
+package masoki.met
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MovementExpansionTestamentDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
